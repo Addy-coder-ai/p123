ec249207c3219e5156a0330e4d7cdbe8a4dc4c6b
Addy-coder-ai <80392675+Addy-coder-ai@users.noreply.github.com>
2021-08-29 15:04:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup () {
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,400)
+    canvas.position(560,150);
+}
+
+function draw() {
+    background("green");
+}
